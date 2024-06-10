34cc1a048f4e3c428a05cd12b547631fc7beada3
Easwanth konduru <103351652+Easwanth176@users.noreply.github.com>
2024-06-10 11:30:06 +0530

    connected with deployed backend

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://speakx-backend.onrender.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
